e8799db8d98ed56c9602cc296ba12dd45604ecc5
nsm01l <nsm01l@hotmail.com>
2024-10-28 21:50:52 +0900

    プロジェクト追加

---
+++ Aberto/Form1.Designer.vb
@@ -0,0 +1,70 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナで必要です。
+    'Windows フォーム デザイナを使用して変更できます。  
+    'コード エディタを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Label1 = New Label()
+        NotifyIcon1 = New NotifyIcon(components)
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AllowDrop = True
+        Label1.BackColor = Color.FromArgb(CByte(192), CByte(192), CByte(255))
+        Label1.Dock = DockStyle.Fill
+        Label1.Location = New Point(0, 0)
+        Label1.Margin = New Padding(4, 0, 4, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(126, 111)
+        Label1.TabIndex = 0
+        Label1.Text = "open"
+        Label1.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' NotifyIcon1
+        ' 
+        NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), Icon)
+        NotifyIcon1.Text = "Aberto"
+        NotifyIcon1.Visible = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(126, 111)
+        Controls.Add(Label1)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Margin = New Padding(4, 4, 4, 4)
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "Form1"
+        Text = "Aberto"
+        TopMost = True
+        ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+
+End Class
---
+++ Aberto/My Project/AssemblyInfo.vb
@@ -0,0 +1,9 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります。
+<Assembly: Guid("fcc12228-ecd9-4e07-8667-ce917288d7c0")> 
